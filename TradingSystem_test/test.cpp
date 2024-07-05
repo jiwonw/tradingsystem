3cf4789afff5c466de23286a2d10e9e8d930a7de
j1.woo <j1.woo@samsung.com>
2024-07-05 14:01:54 +0900

    Test Added on Login

--- TradingSystem_test/test.cpp
+++ TradingSystem_test/test.cpp
@@ -2,7 +2,34 @@
 #include "gmock/gmock.h"
 #include "../TradingSystem/TradingSystem.cpp"
 
-TEST(TradingSystemTest, select000)
-{
+class TradingSystemFixture : public testing::Test {
+public:
 	TradingSystem ts;
+	const string ID = "NICKNAME";
+	const string PW = "ABCD1234";
+	MockKiwer MOCK_KIWER;
+	MockNemo MOCK_NEMO;
+};
+
+TEST_F(TradingSystemFixture, Kiwer_login)
+{
+	// Arrange
+	EXPECT_CALL(MOCK_KIWER, login(ID, PW))
+		.Times(1);
+
+	// Act
+	ts.selectStockBroker("Kiwer");
+	ts.login(ID, PW);
+}
+
+
+TEST_F(TradingSystemFixture, Nemo_login)
+{
+	// Arrange
+	EXPECT_CALL(MOCK_NEMO, certification(ID, PW))
+		.Times(1);
+
+	// Act
+	ts.selectStockBroker("Nemo");
+	ts.login(ID, PW);
 }
