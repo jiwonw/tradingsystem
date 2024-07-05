c84db1ea94733d5c32676d6210012f5f1b6bb0b9
Dongjin <triumph0510@gmail.com>
2024-07-05 14:42:04 +0900

    StockBroker Interface 구현

--- TradingSystem/TradingSystem.cpp
+++ TradingSystem/TradingSystem.cpp
@@ -1,25 +1,52 @@
 #include <string>
+#include "KiwerAPI.cpp"
+#include "NemoAPI.cpp"
 using namespace std;
+
+#define interface struct
+
+interface StockBrokerDriver {
+	virtual void login(string id, string password) = 0;
+	virtual void buy(string stockCode, int price, int count) = 0;
+	virtual void sell(string stockCode, int price, int count) = 0;
+	virtual void getPrice(string stockCode, int minute) = 0;
+};
+
+class KiwerDriver : public StockBrokerDriver {
+	void login(string id, string password);
+	void buy(string stockCode, int price, int count);
+	void sell(string stockCode, int price, int count);
+	void getPrice(string stockCode, int minute);
+};
+
+class NemoDriver : public StockBrokerDriver {
+	void login(string id, string password);
+	void buy(string stockCode, int price, int count);
+	void sell(string stockCode, int price, int count);
+	void getPrice(string stockCode, int minute);
+};
 
 class TradingSystem
 {
 public:
 	void selectStockBroker(string Name) {
-
+		if ("Kiwer") driver = new KiwerDriver();
+		if ("Nemo") driver = new NemoDriver();
+		driver = nullptr;
 	}
 	void login(string id, string password ) {
-
+		driver->login(id, password);
 	}
 	void buy(string stockCode, int price, int count) {
-
+		driver->buy(stockCode, price, count);
 	}
 	void sell(string stockCode, int price, int count) {
-
+		driver->sell(stockCode, price, count);
 	}
-	void getPrice(string stockCode) {
-
+	void getPrice(string stockCode, int minute) {
+		driver->getPrice(stockCode, minute);
 	}
 
 private:
-
+	StockBrokerDriver* driver;
 };
