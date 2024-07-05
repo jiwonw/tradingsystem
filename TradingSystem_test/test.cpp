1bada82e4437af6281cc846d3dba70137e83cf10
j1.woo <j1.woo@samsung.com>
2024-07-05 14:32:44 +0900

    Test Added on sell()

--- TradingSystem_test/test.cpp
+++ TradingSystem_test/test.cpp
@@ -11,6 +11,7 @@ public:
 	MockNemo MOCK_NEMO;
 };
 
+// LOGIN !!!
 TEST_F(TradingSystemFixture, Kiwer_login)
 {
 	// Arrange
@@ -34,6 +35,7 @@ TEST_F(TradingSystemFixture, Nemo_login)
 	ts.login(ID, PW);
 }
 
+// BUY !!!
 TEST_F(TradingSystemFixture, Kiwer_buy)
 {
 	// Arrange
@@ -78,7 +80,6 @@ TEST_F(TradingSystemFixture, Kiwer_buy_without_login)
 	}
 }
 
-
 TEST_F(TradingSystemFixture, Nemo_buy_without_login)
 {
 	// Arrange
@@ -92,5 +93,67 @@ TEST_F(TradingSystemFixture, Nemo_buy_without_login)
 	}
 	catch (exception& e) {
 		EXPECT_EQ(e.what(), string("Cannot buy stock before login"));
+	}
+}
+
+
+// SELL !!!
+TEST_F(TradingSystemFixture, Kiwer_sell)
+{
+	// Arrange
+	EXPECT_CALL(MOCK_KIWER, login(ID, PW))
+		.Times(1);
+	EXPECT_CALL(MOCK_KIWER, sell("SAMSUNG", 100000, 50))
+		.Times(1);
+
+	// Act
+	ts.selectStockBroker("Kiwer");
+	ts.login(ID, PW);
+	ts.sell("SAMSUNG", 100000, 50);
+}
+
+TEST_F(TradingSystemFixture, Nemo_sell)
+{
+	// Arrange
+	EXPECT_CALL(MOCK_NEMO, certification(ID, PW))
+		.Times(1);
+	EXPECT_CALL(MOCK_NEMO, sellingStock("SAMSUNG", 100000, 50))
+		.Times(1);
+
+	// Act
+	ts.selectStockBroker("Nemo");
+	ts.login(ID, PW);
+	ts.buy("SAMSUNG", 100000, 50);
+}
+
+TEST_F(TradingSystemFixture, Kiwer_sell_without_login)
+{
+	// Arrange
+	EXPECT_CALL(MOCK_KIWER, buy("SAMSUNG", 100000, 50))
+		.Times(1);
+
+	// Act
+	try {
+		ts.selectStockBroker("Kiwer");
+		ts.sell("SAMSUNG", 100000, 50);
+	}
+	catch (exception& e) {
+		EXPECT_EQ(e.what(), string("Please login before selling stocks."));
+	}
+}
+
+TEST_F(TradingSystemFixture, Nemo_sell_without_login)
+{
+	// Arrange
+	EXPECT_CALL(MOCK_NEMO, sellingStock("SAMSUNG", 100000, 50))
+		.Times(1);
+
+	// Act
+	try {
+		ts.selectStockBroker("Nemo");
+		ts.sell("SAMSUNG", 100000, 50);
+	}
+	catch (exception& e) {
+		EXPECT_EQ(e.what(), string("Please login before selling stocks."));
 	}
 }
